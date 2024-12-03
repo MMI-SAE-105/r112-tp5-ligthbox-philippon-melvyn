65f6a2a2859058c81da607f7242016d87e780527
philippon-melvyn <melvyn.philippon@edu.univ-fcomte.fr>
2024-12-03 14:59:40 +0100

    java script

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 // *** LIGHTBOX ***
+const lightbox = document.querySelector("#lightbox");
+const lightboximg = document.querySelector("#lightbox > img");
+
+
+document.body.addEventListener("click", (evt) => {
+ if (evt.target.matches("[data-full-img]")) {
+  lightboximg.src = evt.target.dataset.fullImg;
+  lightboximg.showmodal();
+ }
+});
+
+lightbox.addEventListener ("click", (evt)=> {
+lightbox.classList.add("sortie");
+lightbox.addEventListener {
+"animationed",
+()  const lightbox : Element | null
+    lightbox.classList.remove("sortie");
+    lightbox.close; 
+    },
+  {once : true}
+ }
+});
+
+   
