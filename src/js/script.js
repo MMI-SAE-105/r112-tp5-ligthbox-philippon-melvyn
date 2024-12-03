94801adcadc091f522516d0b16255c13b33ffa54
philippon-melvyn <melvyn.philippon@edu.univ-fcomte.fr>
2024-12-03 15:24:04 +0100

    CSS

--- src/js/script.js
+++ src/js/script.js
@@ -4,22 +4,23 @@ const lightboximg = document.querySelector("#lightbox > img");
 
 
 document.body.addEventListener("click", (evt) => {
- if (evt.target.matches("[data-full-img]")) {
-  lightboximg.src = evt.target.dataset.fullImg;
-  lightboximg.showmodal();
- }
+    if (evt.target.matches("[data-full-img]")) {
+        lightboximg.src = evt.target.dataset.fullImg;
+        lightboximg.showmodal();
+    }
 });
 
-lightbox.addEventListener ("click", (evt)=> {
-lightbox.classList.add("sortie");
-lightbox.addEventListener {
-"animationed",
-()  const lightbox : Element | null
-    lightbox.classList.remove("sortie");
-    lightbox.close; 
+lightbox.addEventListener("click", (evt) => {
+    lightbox.classList.add("sortie");
+    lightbox.addEventListener {
+        "animationed",
+    () => {
+    () const lightbox: Element | null
+        lightbox.classList.remove("sortie");
+        lightbox.close;
     },
-  {once : true}
- }
+    { once: true }
+}
 });
 
-   
+
